c0a2fd1a9d73a320277f759154fee4727cc6cb53
brianschnee <brian.a.schnee@gmail.com>
2022-08-06 13:45:06 -0400

    set up handlebars template engine

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+// show requests in console when in development mode
+const morgan = require('morgan')
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
 // load config
 const dotenv = require('dotenv')
 dotenv.config({ path: './config/config.env' })
@@ -8,6 +14,11 @@ dotenv.config({ path: './config/config.env' })
 // connect to DB
 const connectDB = require('./config/db')
 connectDB()
+
+// use express handlebars template engine
+const { engine } = require('express-handlebars');
+app.engine('.hbs', engine({ extname: '.hbs', defaultLayout: 'main' }))
+app.set('view engine', '.hbs')
 
 const PORT = process.env.PORT || 3000
 
