3dd7a2608c5ea09d11e3c5c662ed709884370d94
brianschnee <brian.a.schnee@gmail.com>
2022-08-06 13:11:17 -0400

    connect to db

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
-const dotenv = require('dotenv')
 
 // load config
+const dotenv = require('dotenv')
 dotenv.config({ path: './config/config.env' })
+
+// connect to DB
+const connectDB = require('./config/db')
+connectDB()
+
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
--- config/db.js
+++ config/db.js
@@ -3,15 +3,14 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewURLParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         })
 
         console.log(`MongoDB Connected ${conn.connection.host}`)
     } catch(err) {
         console.error(err)
-        process.exit(1) // 
+        process.exit(1)
     }
 }
 
